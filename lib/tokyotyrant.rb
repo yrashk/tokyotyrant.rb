8a1daddcc2dd849edcf2b9f5d0e517c60765ba3d
Yurii Rashkovskii <yrashk@idbns.com>
2008-07-10 00:39:40 -0700

    improved put command (keep & cat modes), get command

--- lib/tokyotyrant.rb
+++ lib/tokyotyrant.rb
@@ -1,4 +1,5 @@
 require 'socket'
+
 module TokyoTyrant
   
   class Database
@@ -7,12 +8,37 @@ module TokyoTyrant
       @socket = TCPSocket.open(host, port)
     end
 
-    def put(key, value)
-      cmd = [0xc8,0x10].pack('C2') + [key.length, value.length].pack('N2') + key + value
+    def put(key, value, mode = nil)
+      mod = 0x10
+      case mode
+      when :keep
+        mod = 0x11
+      when :cat
+        mod = 0x12
+      end
+      cmd = [0xc8,mod].pack('C2') + [key.length, value.length].pack('N2') + key + value
       @socket.write(cmd)
+      unless (code = @socket.read(1).unpack('C').first) == 0
+        raise "TokyoTyrant error: #{code}"
+      end
+      value
+    end
+
+    alias :[]= :put
+    
+    def get(key)
+      cmd = [0xc8,0x30].pack('C2') + [key.length].pack('N') + key
+      @socket.write(cmd)
+      unless (code = @socket.read(1).unpack('C').first) == 0
+        raise "TokyoTyrant error: #{code}"
+      end
+      len = @socket.read(4).unpack('N').first
+      @socket.read(len)
     end
     
-    alias :[]= :put
+    alias :[] :get
+    
+
     
   end
   
